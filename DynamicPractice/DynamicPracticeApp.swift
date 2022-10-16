f3834b861a1b95eeb8b1c78f6ee778f6cdab718b
patelarth0311 <patelarth0311@gmail.com>
2022-10-16 01:26:57 -0400

    Initial Commit

---
+++ DynamicPractice/DynamicPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicPracticeApp.swift
+//  DynamicPractice
+//
+//  Created by Arth Patel on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct DynamicPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
